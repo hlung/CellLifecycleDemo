dcb52784b6a71436d7aa2a105a86bdcdc25a7385
Thongchai Kolyutsakul <thongchai.kolyutsakul@viki.com>
2024-04-18 12:57:20 +0800

    Initial Commit

---
+++ CellLifecycleDemo/CellLifecycleDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CellLifecycleDemoApp.swift
+//  CellLifecycleDemo
+//
+//  Created by Kolyutsakul, Thongchai on 18/4/24.
+//
+
+import SwiftUI
+
+@main
+struct CellLifecycleDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
